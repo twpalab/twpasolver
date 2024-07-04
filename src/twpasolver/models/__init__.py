3243ac436114d536c4b4fe5752cf0146a17e3382
pietro <campana.pietro@gmail.com>
2024-07-04 10:09:40 +0200

    keep correct ordering of model imports

--- src/twpasolver/models/__init__.py
+++ src/twpasolver/models/__init__.py
@@ -1,5 +1,6 @@
 """Models module."""
-
+# !/bin/python3
+# isort: skip_file
 from twpasolver.models.base_components import Capacitance, Inductance, Stub
-from twpasolver.models.model_arrays import TWPA, AnyModel, ModelArray
 from twpasolver.models.twpa_cells import LCLfBaseCell, StubBaseCell
+from twpasolver.models.model_arrays import TWPA, AnyModel, ModelArray
